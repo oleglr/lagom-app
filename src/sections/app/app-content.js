8410d06a94e9f1e756af65cf093e478870cd1ffc
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2019-12-22 14:19:52 +0800

    add emotion styled components

--- src/sections/app/app-content.js
+++ src/sections/app/app-content.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { UserContext } from '../../context/user-context'
+import styled from '@emotion/styled'
+
+const MainSection = styled.section`
+  text-align: center;
+`
 
 export const AppContent = () => {
   const { name } = React.useContext(UserContext)
 
-  return <div>Hello {name} welcome to lagom</div>
+  return <MainSection>Hello {name} welcome to lagom</MainSection>
 }
