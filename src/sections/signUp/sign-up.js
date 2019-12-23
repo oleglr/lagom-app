d3dbd445291d654fac50d7b70ca70f22217b0805
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2019-12-23 09:10:53 +0800

    make responsive

--- src/sections/signUp/sign-up.js
+++ src/sections/signUp/sign-up.js
@@ -7,43 +7,71 @@ import legoAnimation from '../../assets/lotties/410-lego-loader.json'
 import { Flex, Lottie } from '../../components/container'
 import { useAuth0 } from '../../react-auth0-spa'
 
-const Hero = styled.section`
+const Hero = styled.div`
   height: 90vh;
+`
+const HeaderWrapper = styled(Flex)`
+  width: 60%;
+  padding-left: 12rem;
+
+  @media (max-width: 1100px) {
+    padding-left: 0;
+  }
+`
+const LandingImageWrapper = styled.div`
+  width: 50%;
+  @media (max-width: 1100px) {
+    display: none;
+  }
+`
+
+const Container = styled.section`
+  max-width: 1200px;
+  margin: 0 auto;
+  margin-top: 8rem;
+
+  @media (max-width: 1100px) {
+    margin-top: 6rem;
+  }
 `
 
 const SignUp_ = ({ ...props }) => {
   const { loginWithRedirect } = useAuth0()
 
   return (
-    <Hero>
-      <Flex style={{ marginTop: '10rem' }}>
-        <Flex column style={{ width: '60%', paddingLeft: '12rem' }}>
-          <Heading as="h1" style={{ textAlign: 'left' }} fontSize="42px">
-            Social media for your social circles that doesn't suck.
-          </Heading>
-          <Text style={{ margin: '16px 0', textAlign: 'left' }}>
-            Connect with your friends and only your friends.
-          </Text>
-          <Flex style={{ marginTop: '16px' }} justify="unset">
-            <Button
-              className="btn-primary"
-              style={{ marginRight: '16px' }}
-              onClick={() => props.history.push('/sign-up')}
-            >
-              Sign up
-            </Button>
-            <Button onClick={() => loginWithRedirect({})} variant="outline">
-              Login
-            </Button>
-          </Flex>
+    <Container>
+      <Hero>
+        <Flex>
+          <HeaderWrapper column>
+            <Heading as="h1" style={{ textAlign: 'left' }} fontSize="42px">
+              Relax and express yourself.
+            </Heading>
+            <Text style={{ margin: '16px 0', textAlign: 'left' }}>
+              A small group based social media, connect with family and close
+              friends. Imagine a whatsapp group with features such as photo
+              albums, plan group activities, split bills.
+            </Text>
+            <Flex justify="unset">
+              <Button
+                className="btn-primary"
+                style={{ marginRight: '16px' }}
+                onClick={() => props.history.push('/sign-up')}
+              >
+                Sign up
+              </Button>
+              <Button onClick={() => loginWithRedirect({})} variant="outline">
+                Login
+              </Button>
+            </Flex>
+          </HeaderWrapper>
+          <LandingImageWrapper>
+            <div style={{ marginTop: '-7rem', paddingRight: '10rem' }}>
+              <Lottie animationData={legoAnimation} height={500} width={300} />
+            </div>
+          </LandingImageWrapper>
         </Flex>
-        <div style={{ width: '50%' }}>
-          <div style={{ marginTop: '-7rem', paddingRight: '10rem' }}>
-            <Lottie animationData={legoAnimation} height={500} width={300} />
-          </div>
-        </div>
-      </Flex>
-    </Hero>
+      </Hero>
+    </Container>
   )
 }
 
