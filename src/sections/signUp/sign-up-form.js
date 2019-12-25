8a98dc82b31efb40b067c671fd87e98eebcf610c
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2019-12-25 15:32:14 +0800

    add signup

--- src/sections/signUp/sign-up-form.js
+++ src/sections/signUp/sign-up-form.js
@@ -8,6 +8,7 @@ import {
   FormHelperText,
 } from '@chakra-ui/core'
 import { Heading, Button, Text } from '@chakra-ui/core'
+import { Formik, Form, Field } from 'formik'
 import { Flex } from '../../components/container'
 import { ReactComponent as FacebookIcon } from '../../assets/svgs/facebook.svg'
 import { ReactComponent as GoogleIcon } from '../../assets/svgs/google.svg'
@@ -46,6 +47,19 @@ const SocialWrapper = styled(Flex)`
   }
 `
 
+function hasLowerCase(str) {
+  return /[a-z]/.test(str)
+}
+function hasUpperCase(str) {
+  return /[A-Z]/.test(str)
+}
+function hasNumber(str) {
+  return /[0-9]/.test(str)
+}
+function hasSpecialCharacter(str) {
+  return /[ !@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(str)
+}
+
 export const SignUpForm = () => {
   const { loginWithRedirect } = useAuth0()
 
@@ -55,30 +69,89 @@ export const SignUpForm = () => {
         <Heading size="lg" textAlign="center">
           Join Lagom
         </Heading>
-        <FormControl style={{ padding: '0 2rem' }}>
-          <FormLabel mt={8} htmlFor="email">
-            Email address
-          </FormLabel>
-          <Input type="email" id="email" aria-describedby="email-helper-text" />
-          <FormHelperText id="email-helper-text">
-            We'll never share your email.
-          </FormHelperText>
-          <FormLabel mt={4} htmlFor="password">
-            Password
-          </FormLabel>
-          <Input type="password" id="password" />
-          <Flex justify="flex-end">
-            <Button
-              width="100px"
-              mt={8}
-              className="btn-primary"
-              type="submit"
-              id="form-submit-btn"
-            >
-              Sign up
-            </Button>
-          </Flex>
-        </FormControl>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validate={values => {
+            const errors = {}
+            if (!values.email) {
+              errors.email = 'Required'
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = 'Invalid email address'
+            }
+            if (!values.password) {
+              errors.password = 'Required'
+            } else if (!hasLowerCase(values.password)) {
+              errors.password = 'Include lower case character'
+            } else if (!hasUpperCase(values.password)) {
+              errors.password = 'Include upper case character'
+            } else if (!hasNumber(values.password)) {
+              errors.password = 'Include number'
+            } else if (!hasSpecialCharacter(values.password)) {
+              errors.password = 'include special character'
+            }
+            return errors
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            console.log('submit: ', values)
+            setSubmitting(true)
+            setSubmitting(false)
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <Field
+                name="email"
+                render={({ field, form }) => (
+                  <FormControl
+                    isInvalid={form.errors.email && form.touched.email}
+                  >
+                    <FormLabel mt={8} htmlFor="email">
+                      Email address
+                    </FormLabel>
+                    <Input
+                      {...field}
+                      type="email"
+                      id="email"
+                      aria-describedby="email-helper-text"
+                    />
+                    <FormHelperText id="email-helper-text">
+                      We'll never share your email.
+                    </FormHelperText>
+                    <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                  </FormControl>
+                )}
+              />
+              <Field
+                name="password"
+                render={({ field, form }) => (
+                  <FormControl
+                    isInvalid={form.errors.password && form.touched.password}
+                  >
+                    <FormLabel mt={4} htmlFor="password">
+                      Password
+                    </FormLabel>
+                    <Input {...field} type="password" id="password" />
+                    <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+                  </FormControl>
+                )}
+              />
+              <Flex justify="flex-end">
+                <Button
+                  width="100px"
+                  mt={8}
+                  className="btn-primary"
+                  type="submit"
+                  id="form-submit-btn"
+                  disabled={isSubmitting}
+                >
+                  Sign up
+                </Button>
+              </Flex>
+            </Form>
+          )}
+        </Formik>
         <Text mt={4} mb={4} textAlign="center" color="var(--grey)">
           Or sign up with
         </Text>
